cb88e89ec3e97dde8cdd972290a3f82343e61c1e
Dan Ravensloft <dan.ravensloft@gmail.com>
2018-11-24 19:48:34 +0000

    AddIntcHandler syscall stub

--- ee/thread.c
+++ ee/thread.c
@@ -2,35 +2,8 @@
 
 #include "locations.h"
 #include "mem.h"
+#include "thread.h"
 #include "types.h"
-
-// Register state for a thread.
-struct ThreadContext {
-    u128 gprs[25]; // $at - $t9
-    u128 gp, fp, sp, ra, hi, lo;
-    u32  sa;
-    u32  fprs[32]; // $f0 - $f31
-    u32  fp_acc;
-    u32  fp_ctrl;
-};
-
-// A thread.
-struct Thread {
-    // The thread's $gp register.
-    u32   gp_reg;
-    // Size of the thread's stack.
-    i32   stack_size;
-    // The top of the thread's stack, minus space for a ThreadContext.
-    void* initial_stack;
-    // The bottom of the thread's stack.
-    void* stack_bottom;
-    // The top of the thread's heap.
-    void* heap_top;
-    // The address to return to after the thread exits.
-    void* return_address;
-    // Thread arguments.
-    char* args;
-};
 
 // The thread state. 
 #define END_OF_RAM   0x02000000
--- include/types.h
+++ include/types.h
@@ -1,5 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+#ifndef TYPES_H
+#define TYPES_H
+
 typedef _Bool bool;
 typedef unsigned short u16;
 typedef signed short i16;
@@ -7,3 +10,5 @@ typedef unsigned int u32;
 typedef signed int i32;
 typedef unsigned int u64;
 typedef unsigned int u128 __attribute__(( mode(TI) ));
+
+#endif // TYPES_H
